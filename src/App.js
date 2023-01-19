323b7ec9ec3da8c1dc309b1ecd66bf2c5315b2df
Sasha <sasha@gelato.digital>
2023-01-19 10:02:23 +0100

    updated title and favicon

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import NavBar from "./components/NavBar";
 import Body from "./components/Body";
 
 import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+import useTitle from "./hooks/useTitle";
 
 function App() {
   const desiredChainId = ChainId.Mumbai;
-
+  useTitle("create-gasless-app");
   return (
     <ThirdwebProvider desiredChainId={desiredChainId}>
       <div className="App bg-slate-600 h-screen flex flex-col content-center">
